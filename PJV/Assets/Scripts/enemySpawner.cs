2badd14ecee0eae7fe8d32a88faf0c58f5e86ca0
Eise <eise.booij@gmail.com>
2022-06-01 17:37:27 +0200

    Fixed NavMesh and Added Spawnpoints

--- PJV/Assets/Scripts/enemySpawner.cs
+++ PJV/Assets/Scripts/enemySpawner.cs
@@ -6,6 +6,12 @@ public class enemySpawner : MonoBehaviour
 {
    [ SerializeField ]
     private GameObject enemyPrefab;
+    public GameObject Spawnpoint01;
+    public GameObject Spawnpoint02;
+    public GameObject Spawnpoint03;
+    public GameObject CurrentSpawnpoint;
+
+
 
     public float enemyInterval = 3.5f;
     // Start is called before the first frame update
@@ -20,10 +26,32 @@ public class enemySpawner : MonoBehaviour
         
     }
 
+    private void SpawnpointRandomizer()
+    {
+         int i = Random.Range(1, 3);
+        Debug.Log(i);
+        switch (i) 
+        {
+            case 1:
+                CurrentSpawnpoint = Spawnpoint01;
+                break;
+
+            case 2:
+                CurrentSpawnpoint = Spawnpoint02;
+                break;
+
+            case 3:
+                CurrentSpawnpoint = Spawnpoint03;
+                break;
+        }
+    }
+
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
+       
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6), 0), Quaternion.identity);
+        SpawnpointRandomizer();
+        GameObject newEnemy = Instantiate(enemy, CurrentSpawnpoint.transform.position, Quaternion.identity);
         StartCoroutine(spawnEnemy( interval, enemy));
     }
 }
