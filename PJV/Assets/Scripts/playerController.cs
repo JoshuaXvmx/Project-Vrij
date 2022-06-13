1ecd7696a0495c42f07d45b4d23b1d0f17bb0e17
Eise <eise.booij@gmail.com>
2022-06-13 19:40:05 +0200

    Reworked Snore System (Big Snore doesnt work yet)

--- PJV/Assets/Scripts/playerController.cs
+++ PJV/Assets/Scripts/playerController.cs
@@ -9,47 +9,78 @@ public class playerController : MonoBehaviour
     public float horizontalInput;
 
     public int SnoreRadius;
-    public int SnoreDamage;
+    public int SmallSnoreDamage;
+    public int BigSnoreDamage;
     public int SnoreCooldown;
+    public AudioSource PlayerAudio;
+    public AudioClip SmallSnore;
+    public AudioClip BigSnore;
     private SphereCollider SnoreAttack;
 
     public GameObject Enemyhandler;
- 
-
-
     public LayerMask Enemies;
 
     private List<GameObject> EnemiesInrange = new List<GameObject>();
 
-    IEnumerator DamageRoutine()
-    {
-        Snore();
-
-        yield return new WaitForSeconds(SnoreCooldown); //amount of seconds to wait before next damage
-    }
+    private bool InCooldown;
+    private float StartTime;
+    private float CooldownTime;
+    public float CooldownLength;
+    private bool Counting;
 
     void Start()
     {
-        UpdateRadius();
+        PlayerAudio.clip = SmallSnore;
+        Counting = false;
+        CooldownTime = Time.time;
     }
 
 
-    void Update()
+    void Update() 
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.forward* Time.deltaTime *speed);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
         //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
 
+        if (Time.time - CooldownTime >= CooldownLength)
+        {
+            InCooldown = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            StartCoroutine(DamageRoutine());
+            if (InCooldown != true)
+            {
+                if (Counting == false)
+                {
+                    StartTime = Time.time;
+                    Counting = true;
+                }
+                
+                
+
+                if(Input.GetKeyDown(KeyCode.Space) && Time.time - StartTime >= 2f)   //This one doesnt work yet
+                {
+                    Debug.Log("BigSnore");
+                    PlayerAudio.clip = BigSnore;
+                    BigSnoreAttack();
+                    CooldownTime = Time.time;
+                    Counting = false;
+                    InCooldown = true;
+                }
+            }
 
         }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            StopCoroutine(DamageRoutine());
+            if(InCooldown != true)
+            {
+                Counting = false;
+                PlayerAudio.clip = SmallSnore;
+                SmallSnoreAttack();
+            }
         }
     }
 
@@ -58,14 +89,28 @@ public class playerController : MonoBehaviour
         SnoreAttack.radius = SnoreRadius;
     }
 
-    void Snore()
+    void SmallSnoreAttack()
     {
+        PlayerAudio.Play();
         Enemyhandler.GetComponent<enemySpawner>().UpdateEnemies();
 
         Collider[] EnemiesInRange = Physics.OverlapSphere(this.transform.position, SnoreRadius, Enemies);
         foreach (var hitCollider in EnemiesInRange)
         {
-            hitCollider.GetComponentInParent<EnemyScript>().TakeDamage(SnoreDamage);
+            hitCollider.GetComponentInParent<EnemyScript>().TakeDamage(SmallSnoreDamage);
+
+        }
+    }
+
+    void BigSnoreAttack()
+    {
+        PlayerAudio.Play();
+        Enemyhandler.GetComponent<enemySpawner>().UpdateEnemies();
+
+        Collider[] EnemiesInRange = Physics.OverlapSphere(this.transform.position, SnoreRadius, Enemies);
+        foreach (var hitCollider in EnemiesInRange)
+        {
+            hitCollider.GetComponentInParent<EnemyScript>().TakeDamage(BigSnoreDamage);
 
         }
     }
